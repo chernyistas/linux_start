d8329a9234033fbac6dcc5138e351a2920f53a7c
chernyistas <itsmechernyi@gmail.com>
2026-09-07 12:17:44 +0300

    День 11-12: Добавлены метрики Flask, правила алертов(перпентили, ошибки 5xx), настройка Flertmanager

--- app/app.py
+++ app/app.py
@@ -2,8 +2,15 @@ from flask import Flask
 import os
 import psycopg2
 from datetime import datetime
+from prometheus_flask_exporter import PrometheusMetrics
+# import random
 
 app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+
+from prometheus_client import Counter
+REQUESTS = Counter('my_endpoint_requests_total', 'Total requests to /db endpoint')
 
 @app.route('/')
 def hello():
@@ -11,6 +18,9 @@ def hello():
 
 @app.route('/db')
 def db_check():
+    REQUESTS.inc()
+   # if random.random() < 0.3:
+   #     return "Simulated DB error", 500
     try:
         conn = psycopg2.connect(
             dbname=os.environ.get('POSTGRES_DB'),
